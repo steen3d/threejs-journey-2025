782f5a620e2119e1d37a2f55666ec222b2c95b62
Chris Steenerson <csteenerson@Chriss-MacBook-Pro.local>
2025-02-24 21:34:47 -0500

    Add cube button

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -21,7 +21,23 @@ debugObject.createSphere = () =>
         }
     )
 }
+
+debugObject.createBox = () => 
+    {
+        createBox(
+            Math.random(), 
+            Math.random(), 
+            Math.random(), 
+            {
+                x: Math.random() - 0.5 * 3, 
+                y: 3, 
+                z: Math.random() - 0.5 * 3
+            }
+        )
+    }
+
 gui.add(debugObject, 'createSphere')
+gui.add(debugObject, 'createBox')
 
 /**
  * Base
@@ -194,20 +210,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const objectsToUpdate = [];
 
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4, 
+        envMap: environmentMapTexture
+})
 
 const createSphere = (radius, position) =>
 {
     // Three.js mesh
-    const mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(radius, 20, 20),
-        new THREE.MeshStandardMaterial({
-            metalness: 0.3,
-            roughness: 0.4,
-            envMap: environmentMapTexture
-
-        })
-    )
+    const mesh = new THREE.Mesh( sphereGeometry, sphereMaterial )
     mesh.castShadow = true;
+    mesh.scale.set( radius, radius, radius );
     mesh.position.copy(position)
     scene.add(mesh);
 
@@ -234,6 +249,42 @@ createSphere(0.5, {x: 0, y: 3, z:0})
 
 console.log(objectsToUpdate)
 
+// Create Box
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5
+})
+
+// Three.js mesh
+const createBox = (width, height, depth, position) => {
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    mesh.scale.set(width, height, depth);
+    mesh.castShadow = true;
+    mesh.position.copy(position);
+    scene.add(mesh);
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5));
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position);
+    world.addBody(body);
+
+    // Save in objects to update
+    objectsToUpdate.push({
+        mesh,
+        body
+    }) 
+
+}
 /**
  * Animate
  */
@@ -255,6 +306,7 @@ const tick = () =>
     for(const object of objectsToUpdate)
     {
         object.mesh.position.copy(object.body.position);
+        object.mesh.quaternion.copy(object.body.quaternion);
     }
 
     // sphere.position.copy(sphereBody.position)
